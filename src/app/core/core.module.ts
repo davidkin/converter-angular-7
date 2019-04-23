6b69b03d5c28378a4fadf159114bc12063f1acce
David <davidfloyd322@gmail.com>
2019-04-23 16:54:34 +0300

    master, init

---
+++ src/app/core/core.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreModule } from '@ngrx/store';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    EffectsModule,
+    StoreModule
+  ]
+})
+export class CoreModule { }
